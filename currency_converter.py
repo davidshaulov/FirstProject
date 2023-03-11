38083b6526610bbb9d4f26277afc2332c4dd672b
david <davidshaulov565@gmail.com>
2023-03-11 19:12:03 +0200

    added changes

--- currency_converter.py
+++ currency_converter.py
@@ -1,51 +1,39 @@
-class ILS:
-
-    def __init__(self, value):
-        self.value = value
-
-    @staticmethod
-    def get_value():
-        return 0.28
-
-    def calculate(self, choice_input):
-        return choice_input * self.get_value()
-
-
-class USD:
-
-    def __init__(self, value):
-        self.value = value
-
-    @staticmethod
-    def get_value():
-        return 3.52
-
-    def calculate(self, choice_input):
-        return choice_input * self.get_value()
+def check_amount():
+    try:
+        value_to_convert = float(input('please enter an amount to convert - '))
+        return value_to_convert
+    except ValueError:
+        return False
 
 
 def get_user_value():
     print("Welcome to currency converter \nPlease choose an option (1/2): ")
     choice_input = input("1. Dollars to Shekel \n2. Shekel to Dollars\n")
-
     while True:
         if choice_input == '1':
-            value_to_convert = float(input('please enter an amount to convert'))
-            print("good")
-            shekel = ILS()
+
+            if not check_amount():
+                print("your input wasn't a number let's try again")
+                break
+            else:
+                value_to_convert = check_amount()
+                shekel = ILS()
+                shekel.calculate(value_to_convert)
+
         elif choice_input == '2':
-            value_to_convert = float(input('please enter an amount to convert'))
-            print("good")
-            dollar = USD()
+
+            if not check_amount():
+                print("your input wasn't a number let's try again")
+                break
+            else:
+                value_to_convert = check_amount()
+                dollar = USD()
+                dollar.calculate(value_to_convert)
+
         else:
             choice_input = input("invalid choice, please try again\n1. Dollars to Shekel \n2. Shekel to Dollars\n")
 
-
-if __name__ == '__main__':
-
+    get_user_value()
 
 
-
-
-
-
+get_user_value()
